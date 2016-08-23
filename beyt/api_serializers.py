cebf64ea0ed4aa10d8fbd4b5bd66cf2e3ba88e1e
thibaultsc <thibaultsc@gmail.com>
2016-08-23 17:41:26 -0400

    filter nested lg - best option

--- beyt/api_serializers.py
+++ beyt/api_serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers, viewsets, generics
+from django.db.models import Q
 
 from .models import *
 
@@ -10,16 +11,10 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         model = User
         fields = ('url', 'username', 'email', 'is_staff')
         
-#FilteredList Serializers to filter a List that should be serialized
-class FilteredListSerializer(serializers.ListSerializer):
-    def to_representation(self, data):
-        data = data.filter(type = '1')
-        return super(FilteredListSerializer, self).to_representation(data)
-class TranslateTourSerializer(serializers.HyperlinkedModelSerializer):
+
+class TranslateTourSerializer(serializers.ModelSerializer):
     class Meta:
-        list_serializer_class = FilteredListSerializer
         model = TranslateTour
-        fields = ('title', 'description', 'slug', 'language', 'type')
 
 #LocationSerializer
 class LocationSerializer(serializers.HyperlinkedModelSerializer):
@@ -41,9 +36,13 @@ class TourActivitySerializer(serializers.HyperlinkedModelSerializer):
     
 # TOUR Serializers define the API representation.
 class TourSerializer(serializers.HyperlinkedModelSerializer):
-    locales = TranslateTourSerializer(many=True)
+    locales = serializers.SerializerMethodField('get_rigth_language')
     departureLocation = LocationSerializer(read_only=True)
     activities = TourActivitySerializer(many=True,read_only=True)
+    def get_rigth_language(self, obj):
+        translate_tour = TranslateTour.objects.filter(Q(tour = obj) & (Q(language=self.context['request'].query_params['a']) | Q(type='1')))
+        serializer = TranslateTourSerializer(translate_tour, many=True)
+        return serializer.data
     class Meta:
         model = Tour
     
